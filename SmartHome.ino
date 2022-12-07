56e53eb39ff30e25798dba63b436d0a7c6593204
Fadi Sarwat <fadisarwat@yahoo.com>
2022-12-07 14:43:17 +0200

    WIP

--- SmartHome.ino
+++ SmartHome.ino
@@ -1,10 +1,20 @@
 
+int IR1 = A4;
+
 int led = 13;
 
 void setup() {
+  Serial.begin(115200);
+  
+  pinMode(IR1, INPUT);
+
   pinMode(led, OUTPUT);
 }
 
 void loop() {
+  int sensorStatus = digitalRead(IR1);
 
+  if(sensorStatus==1) {
+    digitalWrite(led, ! digitalRead(led));
+  }
 }
