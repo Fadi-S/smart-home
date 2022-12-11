8ef404fd69cd3aee406c5486b7b857e36e4f5dc4
Fadi Sarwat <fadisarwat@yahoo.com>
2022-12-12 00:15:23 +0200

    Add authentication

--- SmartHome.ino
+++ SmartHome.ino
@@ -1,10 +1,18 @@
 #include "Ultrasound.h"
 #include "Wifi.h"
 
-//Ultrasound echo(2, 3);
+#include "Buzzer.h"
+
+//Ultrasound echo(14, 4);
 int distance;
 
 Wifi wifi("Smart home", "password1234");
+
+Buzzer buzzer(4);
+
+bool loggedIn = false;
+const String password = "1234";
+int failedAttempts = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -15,8 +23,40 @@ void setup() {
 }
 
 void loop() {
+  if(failedAttempts >= 3) {
+    buzzer.toggle(500, 5);
+    failedAttempts--;
+  }
+
   String response = wifi.getResponse();
   if(response == "\0") {
+    return;
+  }
+
+  if(!response.startsWith("/login") && !loggedIn) {
+    wifi.setResponse("You are not logged In!");
+
+    return;
+  }
+
+  if(response.startsWith("/login")) {
+    int start = response.indexOf("password=");
+
+    String inputPassword = response.substring(start+9, response.length());
+
+    if(inputPassword != password) {
+      wifi.setResponse("Wrong password");
+      failedAttempts++;
+      return;
+    }
+
+    loggedIn = true;
+    failedAttempts = 0;
+    buzzer.startFor(200);
+    wifi.setResponse("Logged In");
+
+    // TODO Open door
+
     return;
   }
 
@@ -27,6 +67,11 @@ void loop() {
 
   if(response == "/getTemperature") {
     wifi.setResponse("25");
+    return;
+  }
+
+  if(response == "/logout") {
+    loggedIn = false;
     return;
   }
 
